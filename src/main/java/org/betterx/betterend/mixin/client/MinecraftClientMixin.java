5102e304e90c2058dd252ae6c5072f00bb2ac150
RedCraft86 <67089499+RedCraft86@users.noreply.github.com>
2026-02-02 22:37:30 -0500

    Cache non-replacing end music to avoid excessive allocations

--- src/main/java/org/betterx/betterend/mixin/client/MinecraftClientMixin.java
+++ src/main/java/org/betterx/betterend/mixin/client/MinecraftClientMixin.java
@@ -2,6 +2,7 @@ package org.betterx.betterend.mixin.client;
 
 import org.betterx.bclib.util.MHelper;
 
+import net.minecraft.client.main.GameConfig;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
@@ -15,12 +16,17 @@ import net.minecraft.world.level.Level;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Minecraft.class)
 public class MinecraftClientMixin {
+    @Unique
+    private static Music END_MUSIC = null;
+
     @Shadow
     public LocalPlayer player;
 
@@ -34,6 +40,25 @@ public class MinecraftClientMixin {
     @Shadow
     public ClientLevel level;
 
+    @Unique
+    private static Music be_getOrCacheEndMusic() {
+        if (END_MUSIC == null) {
+            END_MUSIC = new Music(
+                    Musics.END.getEvent(),
+                    Musics.END.getMinDelay(),
+                    Musics.END.getMaxDelay(),
+                    false // Don't replace current music
+            );
+        }
+        return END_MUSIC;
+    }
+
+    @Inject(method = "<init>*", at = @At(value = "TAIL"))
+    private void onInit(GameConfig args, CallbackInfo info) {
+        be_getOrCacheEndMusic();
+    }
+
+
     @Inject(method = "getSituationalMusic", at = @At("HEAD"), cancellable = true)
     private void be_getEndMusic(CallbackInfoReturnable<Music> info) {
         if (!(this.screen instanceof WinScreen) && this.player != null) {
@@ -44,20 +69,9 @@ public class MinecraftClientMixin {
                 ) < 250000) {
                     info.setReturnValue(Musics.END_BOSS);
                 } else {
-                    Music sound = this.level.getBiomeManager()
-                                            .getNoiseBiomeAtPosition(this.player.blockPosition())
-                                            .value()
-                                            .getBackgroundMusic()
-                                            .orElse(null);
-                    if (sound == null) {
-                        sound = new Music(
-                                Musics.END.getEvent(),
-                                Musics.END.getMinDelay(),
-                                Musics.END.getMaxDelay(),
-                                false
-                        );
-                    }
-                    info.setReturnValue(sound);
+                    info.setReturnValue(this.level.getBiomeManager()
+                            .getNoiseBiomeAtPosition(this.player.blockPosition()).value()
+                            .getBackgroundMusic().orElse(be_getOrCacheEndMusic()));
                 }
                 info.cancel();
             }
